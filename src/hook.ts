14e35679be9e7fd103bf484211a536067e1edb66
超杰 <1098626505@qq.com>
2023-01-18 22:02:17 +0800

    fix: hook add owner

--- src/hook.ts
+++ src/hook.ts
@@ -1,30 +1,27 @@
+import { createRoot, getOwner, Owner } from "solid-js";
 
-
-
-import { createRoot, Owner } from "solid-js";
-
-export function runAsyncHook(cb: () => Promise<void>, owner?: Owner | undefined | null): Promise<void> {
+export function runAsyncHook(cb: (owner: Owner | null) => Promise<void>, owner?: Owner | null): Promise<void> {
   return new Promise((resolve, reject) => {
-    createRoot(async () => {
-      try {
-        const consoleWarn = cy.spy(console, 'warn')
-        await cb()
+    createRoot(() => {
+      const owner = getOwner()
+      const consoleWarn = cy.spy(console, 'warn')
+      Promise.resolve(cb(owner)).then(() => {
         if (consoleWarn.calledWith('computations created outside a `createRoot` or `render` will never be disposed')) {
           expect(consoleWarn).not.be.calledWith('computations created outside a `createRoot` or `render` will never be disposed')
+          reject();
+        } else {
+          resolve()
         }
-        consoleWarn.restore()
-        resolve()
-      } catch (err) {
-        reject(err)
-      }
+      }, reject)
     }, owner!)
   })
 }
 
-export const runHook = (cb: () => void, owner?: Owner | undefined | null) => {
+export const runHook = (cb: (owner: Owner | null) => void, owner?: Owner | null) => {
   createRoot(() => {
     const consoleWarn = cy.spy(console, 'warn')
-    cb()
+    const owner = getOwner()
+    cb(owner)
     if (consoleWarn.calledWith('computations created outside a `createRoot` or `render` will never be disposed')) {
       expect(consoleWarn).not.be.calledWith('computations created outside a `createRoot` or `render` will never be disposed')
     }
